2dd886a387fd284df5b0762ec41ccb134cf8d27a
Stanjustine <stanjustine@gmail.com>
2020-03-05 13:27:17 -0800

    this is a nice trial

---
+++ script/app.js
@@ -0,0 +1,19 @@
+var counter = true;
+function changeWindow() {
+    document.getElementById("first").classList.toggle("active");
+    document.getElementById("second").classList.toggle("active");
+    document.getElementById("page1").classList.toggle("hide");
+    document.getElementById("page2").classList.toggle("hide");
+}
+
+function slide() {
+
+
+    if(counter){
+        document.getElementById("slide-1").classList.toggle("hide");
+        document.getElementById("slide-2").classList.toggle("hide");
+    }
+
+
+
+}
